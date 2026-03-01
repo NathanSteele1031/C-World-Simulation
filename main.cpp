b70d1c7bc29bc4937274d077637bca6a79c0a729
Nathan Steele <nsteele1031@gmail.com>
2026-02-28 21:24:35 -0500

    Testing all Tile function class.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include "Tile/TileObject/TileObject.h"
+#include "Tile/Tile.h"
 
 using namespace std;
 
 int main(){
     cout << "Hello, World!" << endl;
+    Tile tile;
     TileObject tree("Tree", 5);
+    tile.addObject(&tree);
     cout << tree.getName() << endl;
+    tile.showObjectNames();
+    cout << "Index for tree " << tile.searchObject("Tree") << endl;
+    TileObject givenTree = *tile.giveObject(0);
+    tile.showObjectNames();
+
     return 0;
 }
